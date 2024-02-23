88d343b14cb5fd69dab4d20ee219895d59a94c65
Gaurav Patil <patilgauravajit@gmail.com>
2024-02-23 23:42:14 +0530

    Motormentor v1.0.1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import pymongo
 
 
 app = FastAPI()
@@ -16,10 +17,33 @@ async def root(request:Request):
     return templates.TemplateResponse("login.html",{"request":request})
 
 @app.post('/authenticate')
-async def login(user:str):
-    return ({"user":user})
+async def login(request:Request,email:str = Form(...),password:str = Form(...)):
+    conn = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = conn['Motormentor']
+    collection = db['user_data']
+
+    valid = collection.find_one({"email":email},{'_id':0})
+    if(valid["password"]==password):
+        return templates.TemplateResponse("index.html",{"request":request})
+    else:
+        return ({"data":"not found"})
+
+@app.post('/submit_data')
+async def create(request:Request,email:str = Form(...),password:str = Form(...),phone:int = Form(...)):
+    conn = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = conn['Motormentor']
+    collection = db['user_data']
+    data = {
+        "email":email,
+        "password":password,
+        "phone":phone
+    }
+    collection.insert_one(data)
+    return templates.TemplateResponse("index.html",{"request":request})
+
 
 @app.get('/create_account')
 async def create(request:Request):
     return templates.TemplateResponse("create.html",{"request":request})
     
+   
