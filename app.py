4d6cf255c7bc31b11d3a7e6ab0fc54fd087da3b5
Gaurav Patil <patilgauravajit@gmail.com>
2024-03-12 09:11:07 +0530

    Implement price prediction

--- app.py
+++ app.py
@@ -59,60 +59,6 @@ async def profile(request:Request):
 async def contact(request:Request):
     return templates.TemplateResponse("contact.html",{"request":request})
 
-# @app.websocket('/predict')
-# async def contact(websocket: WebSocket):
-#     await websocket.accept()
-
-#     data = await websocket.receive_text()
-#     data_dict = json.loads(data)
-#     car_name = data_dict.get('car_name')
-#     kmdriven = data_dict.get('kmdriven')
-#     mileage = data_dict.get('mileage')
-#     engine = data_dict.get('engine')
-#     power = data_dict.get('power')
-#     year = data_dict.get('year')
-#     Seats = data_dict.get('Seats')
-#     fuel_type = data_dict.get('fuel_type')
-#     Transmission = data_dict.get('Transmission')
-#     Ownertype = data_dict.get('Ownertype')
-#     def fuel(fuel_type):
-#         if fuel_type == 'Diesel':
-#             return 1
-#         elif fuel_type == 'Petrol':
-#             return 2
-#         elif fuel_type == 'CNG':
-#             return 3
-#         elif fuel_type == 'LPG':
-#             return 4
-#         else:
-#             return 5
-
-#     def Trans(Transmission):
-#         if Transmission == 'Manual':
-#             return 1
-#         else:
-#             return 2
-
-#     def owner(Ownertype):
-#         if Ownertype == 'First':
-#             return 1
-#         elif Ownertype == 'Second':
-#             return 2
-#         elif Ownertype == 'Third':
-#             return 3
-#         else:
-#             return 4
-
-#     await websocket.accept()
-
-#     try:
-#         prediction_result = prediction(year, kmdriven, mileage, engine, power, Seats, fuel(fuel_type), Trans(Transmission), owner(Ownertype), car_name)
-#         await websocket.send_text(str({'prediction_result': prediction_result}))
-#     except Exception as e:
-#         await websocket.send_text(str({'error': str(e)}))
-#     finally:
-#         await websocket.close()
-
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
