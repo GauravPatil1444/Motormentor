2ba8ead650bcf388e7cc505b58687bdd3e5d24d8
Gaurav Patil <patilgauravajit@gmail.com>
2024-03-29 18:28:48 +0530

    Implement model

--- model/model.py
+++ model/model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
 
 df = pd.read_csv('model/processed.csv')
 tdf = pd.read_csv('model/model_data.csv')
@@ -7,10 +8,10 @@ x = df.drop(['Price'],axis='columns')
 le = LabelEncoder()
 x['Name'] = le.fit_transform(tdf.en_Name)
 
-from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor(n_estimators=100)
 
 rf.fit(x,df.Price)
+
 def prediction(year,kmdriven,mileage,engine,power,Seats,fuel_type,Transmission,Ownertype,car_name):
    en_carname = le.transform([car_name.capitalize()])
    res = rf.predict([[year,kmdriven,mileage,engine,power,Seats,fuel_type,Transmission,Ownertype,en_carname[0]]])
